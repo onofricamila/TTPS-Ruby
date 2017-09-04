f2599c9e3106acf02d708d5fa88f64713084a312
Camila <onofricamila2@gmail.com>
2017-09-03 22:33:42 -0300

    p1e3

---
+++ p1/p1e3.rb
@@ -0,0 +1,11 @@
+#	3. Escribí una función llamada reemplazar que reciba un String y que busque y reemplace
+#	en el mismo cualquier ocurrencia de { por do\n y cualquier ocurrencia de } por \nend, de
+#	modo que convierta los bloques escritos con llaves por bloques multilínea con do y end.
+#	Por ejemplo:
+
+#	reemplazar("3.times { |i| puts i }")
+#	# => "3.times do\n |i| puts i \nend"
+
+def reemplazar(string)
+    string.gsub(/[{}]/, '{' => 'do\n', '}' => ' \nend') 
+end
