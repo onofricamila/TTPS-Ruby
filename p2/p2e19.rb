783f660ffaa08e372304d7ed0dcfffb512be9a93
Camila <onofricamila2@gmail.com>
2017-09-28 16:08:38 -0300

    renombres para respetar orden

---
+++ p2/p2e19.rb
@@ -0,0 +1,17 @@
+# 19. Hacer fibbonacci usando Enumerator
+
+fibonacci = Enumerator.new do |caller|
+    i1, i2 = 1, 1
+    loop do
+      caller.yield i1
+      i1, i2 = i2, i1+i2
+    end
+  end
+  
+  6.times { puts fibonacci.next }
+
+  # NOTA:
+  # El método loop 
+  #     - Ejecuta el código que se encuentra dentro del bloque
+  #     - Se puede salir con break cuando se cumple una condición
+  #     - Si hay iteradores, loop terminará cuando el Enumerator se quede sin valores
