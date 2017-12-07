19e628bece6367c693a517319b867f08548562ab
Camila Onofri <onofricamila2@gmail.com>
2017-12-07 10:46:35 -0300

    practica_cinco

--- p5/rails_guide/blog/config/routes.rb
+++ p5/rails_guide/blog/config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  root 'welcome#index'
-  
-  resources :articles
-  
   resources :articles do
     resources :comments
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#index'
 end
