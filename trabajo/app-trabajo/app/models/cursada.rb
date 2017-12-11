b1d6a974399dd50cd2685cd42ea90a12ba14064d
Camila Onofri <onofricamila2@gmail.com>
2017-12-10 21:12:07 -0300

    validacion del año de cursada

--- trabajo/app-trabajo/app/models/cursada.rb
+++ trabajo/app-trabajo/app/models/cursada.rb
@@ -1,2 +1,9 @@
 class Cursada < ApplicationRecord
+    validates :year, presence: true,
+        uniqueness: true,
+        inclusion: { in: 1990..Date.today.year },
+        format: { 
+         with:  /(19|20)\d{2}/i, 
+         message: "should be a four-digit year"
+        }
 end
