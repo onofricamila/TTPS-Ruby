e08efc2c8ebee9bd0e2fca40e42514ad2cf6b8db
Camila Onofri <onofricamila2@gmail.com>
2017-12-11 14:57:49 -0300

    validations in courses done

--- trabajo/app-trabajo/app/models/course.rb
+++ trabajo/app-trabajo/app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ApplicationRecord
+    validates :year, 
+    presence: true,
+    uniqueness: true,
+    inclusion: { in: 1990..Date.today.year ,message: "should be a year from 1990 to current"},
+    format: { 
+      with: /(19|20)\d{2}/i, 
+      message: "should be a four-digit year"
+    }
 end
