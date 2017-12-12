93c2f21c267bf143c6fd5587fbbd91d9c5b707df
Camila Onofri <onofricamila2@gmail.com>
2017-12-12 13:25:52 -0300

    restricciones en la nota y back to courses added

--- trabajo/app-trabajo/app/models/exam.rb
+++ trabajo/app-trabajo/app/models/exam.rb
@@ -2,5 +2,11 @@ class Exam < ApplicationRecord
   belongs_to :course
 
   validates_presence_of :title, :date, :passing_score
+
+  validates :title, 
+    uniqueness: true
+
+  validates :passing_score, 
+    inclusion: { in: 1..100 ,message: "should be a number between 1 and 100, representing a proportion"}
  
 end
