f3ef607753d649c3931b052c31835a23a892addf
Camila Onofri <onofricamila2@gmail.com>
2017-12-12 17:47:47 -0300

    validacion de dni unico en un curso, legajo unico, y nombre-ape unicos en un curso

--- trabajo/app-trabajo/app/models/result.rb
+++ trabajo/app-trabajo/app/models/result.rb
@@ -5,7 +5,9 @@ class Result < ApplicationRecord
   validates_presence_of :student_id, :score
 
   validates_numericality_of :score, :only_integer => true,
-  :greater_than_or_equal_to => 1,
-  :less_than_or_equal_to => 100,
-  :message => "can only be whole number between 1 and 100 standing for a proportion over 100."
+    :greater_than_or_equal_to => 1,
+    :less_than_or_equal_to => 100,
+    :message => "can only be whole number between 1 and 100 standing for a proportion over 100."
+
+  validates :student_id, :uniqueness => {:scope => :exam_id}
 end
--- trabajo/app-trabajo/app/models/student.rb
+++ trabajo/app-trabajo/app/models/student.rb
@@ -6,4 +6,7 @@ class Student < ApplicationRecord
   validates_format_of :email, :with =>/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates_format_of :surname, :name, :with =>/\A(?:[-a-z']+|[-a-z']+\s[-a-z']*\s?[-a-z']+)\z/i 
 
+  validates :dni, :uniqueness => {:scope => [:course_id]}
+  validates :surname, :uniqueness => {:scope => [:course_id, :name]}
+  validates :number, :uniqueness => {:scope => [:course_id]}
 end
