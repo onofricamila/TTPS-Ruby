6a8062f3917873f5ee68a067fc3bf17093c5a6a8
Camila Onofri <onofricamila2@gmail.com>
2017-12-11 23:26:45 -0300

    added algunas validaciones

--- trabajo/app-trabajo/app/models/exam.rb
+++ trabajo/app-trabajo/app/models/exam.rb
@@ -1,3 +1,6 @@
 class Exam < ApplicationRecord
   belongs_to :course
+
+  validates_presence_of :title, :date, :passing_score
+ 
 end
--- trabajo/app-trabajo/app/models/student.rb
+++ trabajo/app-trabajo/app/models/student.rb
@@ -1,3 +1,8 @@
 class Student < ApplicationRecord
   belongs_to :course
+
+  validates_presence_of :surname, :name, :dni, :number, :email
+  validates_format_of :email, :with =>/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates_format_of :surname, :name, :with =>/\A(?:[-a-z']+|[-a-z']+\s[-a-z']*\s?[-a-z']+)\z/i 
+
 end
