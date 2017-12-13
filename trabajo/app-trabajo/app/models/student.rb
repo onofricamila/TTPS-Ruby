6ddbfa21a7fc5db0733cc32642fa15f0e638dc50
Camila Onofri <onofricamila2@gmail.com>
2017-12-13 16:31:07 -0300

    mejoras en validaciones

--- trabajo/app-trabajo/app/models/student.rb
+++ trabajo/app-trabajo/app/models/student.rb
@@ -6,10 +6,12 @@ class Student < ApplicationRecord
   validates_format_of :email, :with =>/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates_format_of :surname, :name, :with =>/\A(?:[-a-z']+|[-a-z']+\s[-a-z']*\s?[-a-z']+)\z/i 
 
-  validates :dni, :uniqueness => {:scope => [:course_id]}
-  validates :surname, :uniqueness => {:scope => [:course_id, :name]}
-  validates :number, :uniqueness => {:scope => [:course_id]}
+  validates_numericality_of :dni, :number, :only_integer => true,
+    :greater_than_or_equal_to => 1
 
+  validates :dni, :number, :email, :uniqueness => {:scope => [:course_id]}
+  validates :name, :uniqueness => {:scope => [:course_id, :surname], message: "should be different, at least. There is someone in this course with that name and surname." }
+  
   def attended_to? exam
     !(Result.find_by_assoc(self, exam).empty?)
   end
