92fe7cd21bda663909f42a844a67c7f3aba3bfc8
Camila Onofri <onofricamila2@gmail.com>
2017-12-15 20:32:19 -0300

    bases testing courses model, fix users fisture

--- trabajo/app-trabajo/test/models/course_test.rb
+++ trabajo/app-trabajo/test/models/course_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # called before every single test
+  
+   setup do
+    @course = courses(:one)
+  end
+
+  test "should not save course with a year < 1990" do
+    @course.year = 1810
+    assert_not @course.save
+  end
 end
