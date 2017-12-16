16c26914b5597f8e5640cec6a4def2236b8b559d
Camila Onofri <onofricamila2@gmail.com>
2017-12-15 22:46:46 -0300

    queda pendiente test de students

--- trabajo/app-trabajo/test/models/student_test.rb
+++ trabajo/app-trabajo/test/models/student_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # called before every single test
+  setup do
+    @student = students(:one)
+  end
+
+  test "should be valid" do
+    assert @student.valid?
+  end
+
 end
