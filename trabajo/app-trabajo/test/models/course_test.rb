1f0634dc787f79aaf5ae89c6d3bc6b7904acdeaf
Camila Onofri <onofricamila2@gmail.com>
2017-12-15 21:12:04 -0300

    testing exam model bases

--- trabajo/app-trabajo/test/models/course_test.rb
+++ trabajo/app-trabajo/test/models/course_test.rb
@@ -2,13 +2,38 @@ require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
   # called before every single test
-  
-   setup do
+  setup do
     @course = courses(:one)
   end
 
-  test "should not save course with a year < 1990" do
-    @course.year = 1810
+  # called after every single test
+  teardown do
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+
+  test "should be valid" do
+    assert @course.valid?
+  end
+
+  test "should not save course without a year" do
+    @course.year = nil
     assert_not @course.save
+  end
+
+  test "year should not be too old" do
+    @course.year = 1800
+    assert_not @course.valid?
+  end
+
+  test "should not save course with a year < 1990" do
+    @course.year = 1800
+    assert_not @course.save
+  end
+
+  test "should not save course with another course's year" do
+    duplicate_course = @course.dup
+    @course.save!
+    assert_not duplicate_course.valid?
   end
 end
--- trabajo/app-trabajo/test/models/exam_test.rb
+++ trabajo/app-trabajo/test/models/exam_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class ExamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  require 'test_helper'
+  
+    # called before every single test
+    setup do
+      @exam = exams(:one)
+    end
+  
+    # called after every single test
+    teardown do
+      # when controller is using cache it may be a good idea to reset it afterwards
+      Rails.cache.clear
+    end
+  
+    test "should be valid" do
+      assert @exam.valid?
+    end
+  
+    test "should not save exam without title" do
+      @exam.title = nil
+      assert_not @exam.save
+    end
+
+    test "should not save exam without date" do
+      @exam.date = nil
+      assert_not @exam.save
+    end
+
+    test "should not save exam without passing_score" do
+      @exam.passing_score = nil
+      assert_not @exam.save
+    end
+  
+    test "passing score should not be negative" do
+      @exam.passing_score = -10
+      assert_not @exam.valid?
+    end
+
+    test "passing score should not be greater than 100" do
+      @exam.passing_score = 200
+      assert_not @exam.valid?
+    end
+  
+    test "should not save exam with another exam's title in the same course" do
+      duplicate_exam = @exam.dup
+      @exam.save!
+      assert_not duplicate_exam.valid?
+    end
+  
 end
