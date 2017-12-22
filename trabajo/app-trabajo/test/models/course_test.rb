bbcbc13071a18c8910aaa3b7cc014dd593721729
Camila Onofri <onofricamila2@gmail.com>
2017-12-22 18:32:09 -0300

    added tests should [not] destroy course. fixtures/courses changed

--- trabajo/app-trabajo/test/models/course_test.rb
+++ trabajo/app-trabajo/test/models/course_test.rb
@@ -11,11 +11,6 @@ class CourseTest < ActiveSupport::TestCase
     assert_not @course.save
   end
 
-  test "year should not be too old" do
-    @course.year = 1800
-    assert_not @course.valid?
-  end
-
   test "should not save course with a year < 1990" do
     @course.year = 1800
     assert_not @course.save
@@ -25,5 +20,13 @@ class CourseTest < ActiveSupport::TestCase
     duplicate_course = @course.dup
     @course.save!
     assert_not duplicate_course.valid?
+  end
+
+  test 'should not destroy course if it has students/exams related' do
+    assert_not @course.destroy
+  end
+
+  test 'should destroy course if it has not students/exams related' do
+    assert courses(:tree).destroy
   end
 end
